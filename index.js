2f19ede5e81e91ac7b9a8a1987d7b85ffcb8033f
Felipe-builder <felipesvascon@gmail.com>
2021-12-23 22:47:38 -0300

    Criacao do Serializador

--- index.js
+++ index.js
@@ -5,8 +5,27 @@ const config = require('config')
 const NaoEncontrado = require('./erros/NaoEncontrado')
 const CampoInvalido = require('./erros/CampoInvalido')
 const DadosNaoFornecidos = require('./erros/DadosNaoFornecidos')
+const ValorNaoSuportado = require('./erros/ValorNaoSuportado')
+const formatosAceitos = require('./Serializador').formatosAceitos
 
 app.use(bodyParser.json())
+
+app.use((req, res, proximo) => {
+    let formatoReq = req.header('Accept')
+
+    if (formatoReq === '*/*') {
+        formatoReq = 'application/json'
+    }
+
+    if (formatosAceitos.indexOf(formatoReq) === -1) {
+        res.status(406)
+        res.end()
+        return
+    }
+
+    res.setHeader('Content-Type', formatoReq)
+    proximo()
+})
 
 const roteadorFilmes = require('./rotas/filmes')
 app.use('/api/filmes', roteadorFilmes)
@@ -22,6 +41,10 @@ app.use((erro, req, res, proximo) => {
 
     if (erro instanceof CampoInvalido || erro instanceof DadosNaoFornecidos) {
         status = 400
+    }
+
+    if (erro instanceof ValorNaoSuportado) {
+        status = 406
     }
 
     res.status(status)
